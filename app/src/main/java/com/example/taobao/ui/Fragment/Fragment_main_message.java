bec5f711008a9b1ee9ddb022b6c2f8290608a5b0
yn030727 <2601098388@qq.com>
2022-11-02 19:56:05 +0800

    2.实现message界面（除了Recyclerview）

--- app/src/main/java/com/example/taobao/ui/Fragment/Fragment_main_message.java
+++ app/src/main/java/com/example/taobao/ui/Fragment/Fragment_main_message.java
@@ -2,6 +2,8 @@ package com.example.taobao.ui.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
